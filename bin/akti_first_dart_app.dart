d83bf39caebccbfce037f7abd9a7055a9bc032b8
Abid <abidroid1@gmail.com>
2024-11-17 12:21:47 +0500

    if else

--- bin/akti_first_dart_app.dart
+++ bin/akti_first_dart_app.dart
@@ -11,82 +11,103 @@ import 'dart:io';
 
 void main()
 {
-  print('Hello World');
-
-  // variable declare
-  int totalStudents;
-
-  // variable initialize
-  totalStudents = 10;
-  print(totalStudents);
-
-  totalStudents = 15;
-  print(totalStudents);
-
-  final double pi = 3.14;
-  print(pi);
-
-  String name = "Arfa Karim";
-  String address = "Street # 13, House # 5, Hayatabad";
-  String city = 'Peshawar';
-  print(name);
-
-  bool feePaid = false;
-
-  num value = 4;
-
-  var university = "CECOS";
-  print(university.runtimeType);
-
-  print('**************************');
-  dynamic a = "AKTI";
-  print(a.runtimeType);
-
-  a = true;
-  a = 5.5;
-  print(a.runtimeType);
-
-  // Optional
-  String? cnic;
-  print(cnic);
-
-  // String
-  String firstName = "Abid";
-  String lastName = "Ali";
-
-  //String fullName = firstName + ' + ' + lastName;
-  // String interpolation
-  String fullName = "My name is $firstName $lastName [ $university ]";
-  print(fullName);
-
-  print( 7 + 5);
-  print( 7 - 5);
-  print( 7 * 5);
-
-  print( 7 / 5);
-  print( 17 ~/ 5);
-
-  print(1 % 5 );
-
-  // String fatherName;
-  // print('Enter your father name : ');
-  // fatherName = stdin.readLineSync()!;
+  // print('Hello World');
   //
+  // // variable declare
+  // int totalStudents;
   //
-  //   print( 'Father name $fatherName');
+  // // variable initialize
+  // totalStudents = 10;
+  // print(totalStudents);
+  //
+  // totalStudents = 15;
+  // print(totalStudents);
+  //
+  // final double pi = 3.14;
+  // print(pi);
+  //
+  // String name = "Arfa Karim";
+  // String address = "Street # 13, House # 5, Hayatabad";
+  // String city = 'Peshawar';
+  // print(name);
+  //
+  // bool feePaid = false;
+  //
+  // num value = 4;
+  //
+  // var university = "CECOS";
+  // print(university.runtimeType);
+  //
+  // print('**************************');
+  // dynamic a = "AKTI";
+  // print(a.runtimeType);
+  //
+  // a = true;
+  // a = 5.5;
+  // print(a.runtimeType);
+  //
+  // // Optional
+  // String? cnic;
+  // print(cnic);
+  //
+  // // String
+  // String firstName = "Abid";
+  // String lastName = "Ali";
+  //
+  // //String fullName = firstName + ' + ' + lastName;
+  // // String interpolation
+  // String fullName = "My name is $firstName $lastName [ $university ]";
+  // print(fullName);
+  //
+  // print( 7 + 5);
+  // print( 7 - 5);
+  // print( 7 * 5);
+  //
+  // print( 7 / 5);
+  // print( 17 ~/ 5);
+  //
+  // print(1 % 5 );
+  //
+  // // String fatherName;
+  // // print('Enter your father name : ');
+  // // fatherName = stdin.readLineSync()!;
+  // //
+  // //
+  // //   print( 'Father name $fatherName');
+  //
+  // int num1, num2, sum;
+  // print('Enter first number : ');
+  // num1 = int.parse( stdin.readLineSync()! );
+  // print('Enter second number : ');
+  // num2 = int.parse( stdin.readLineSync()! );
+  //
+  // sum = num1 + num2;
+  // print('Sum of $num1 and $num2 = $sum');
 
-  int num1, num2, sum;
-  print('Enter first number : ');
-  num1 = int.parse( stdin.readLineSync()! );
-  print('Enter second number : ');
-  num2 = int.parse( stdin.readLineSync()! );
+  int number, remainder;
+  print('Enter a number : ');
+  number = int.parse( stdin.readLineSync()! );
 
-  sum = num1 + num2;
-  print('Sum of $num1 and $num2 = $sum');
+  remainder = number % 2;
 
-
-
+  if( remainder == 1) {
+    print('$number is odd');
+  }else{
+    print('$number is even');
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
