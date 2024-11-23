6f5abe9bfa75406336cf845407ab19d30c5ab59b
Abid <abidroid1@gmail.com>
2024-11-23 13:48:19 +0500

    class

--- bin/akti_first_dart_app.dart
+++ bin/akti_first_dart_app.dart
@@ -7,198 +7,115 @@
   Dart program
  */
 
+
+
 import 'dart:io';
 
-void main() {
-  // print('Hello World');
-  //
-  // // variable declare
-  // int totalStudents;
-  //
-  // // variable initialize
-  // totalStudents = 10;
-  // print(totalStudents);
-  //
-  // totalStudents = 15;
-  // print(totalStudents);
-  //
-  // final double pi = 3.14;
-  // print(pi);
-  //
-  // String name = "Arfa Karim";
-  // String address = "Street # 13, House # 5, Hayatabad";
-  // String city = 'Peshawar';
-  // print(name);
-  //
-  // bool feePaid = false;
-  //
-  // num value = 4;
-  //
-  // var university = "CECOS";
-  // print(university.runtimeType);
-  //
-  // print('**************************');
-  // dynamic a = "AKTI";
-  // print(a.runtimeType);
-  //
-  // a = true;
-  // a = 5.5;
-  // print(a.runtimeType);
-  //
-  // // Optional
-  // String? cnic;
-  // print(cnic);
-  //
-  // // String
-  // String firstName = "Abid";
-  // String lastName = "Ali";
-  //
-  // //String fullName = firstName + ' + ' + lastName;
-  // // String interpolation
-  // String fullName = "My name is $firstName $lastName [ $university ]";
-  // print(fullName);
-//
-  // print( 7 + 5);
-  // print( 7 - 5);
-  // print( 7 * 5);
-  //
-  // print( 7 / 5);
-  // print( 17 ~/ 5);
-  //
-  // print(1 % 5 );
-  //
-  // // String fatherName;
-  // // print('Enter your father name : ');
-  // // fatherName = stdin.readLineSync()!;
-  // //
-  // //
-  // //   print( 'Father name $fatherName');
-  //
-  // int num1, num2, sum;
-  // print('Enter first number : ');
-  // num1 = int.parse( stdin.readLineSync()! );
-  // print('Enter second number : ');
-  // num2 = int.parse( stdin.readLineSync()! );
-  //
-  // sum = num1 + num2;
-  // print('Sum of $num1 and $num2 = $sum');
-
-  // int number, remainder;
-  // print('Enter a number : ');
-  // number = int.parse( stdin.readLineSync()! );
-  //
-  // remainder = number % 2;
-  //
-  // if( remainder == 1) {
-  //   print('$number is odd');
-  // }else{
-  //   print('$number is even');
-  // }
-
-  // int monthNumber;
-  // print('Enter month number : ');
-  // monthNumber = int.parse( stdin.readLineSync()!);
-  //
-  // if( monthNumber == 1){
-  //   print('January');
-  // }
-  // else if( monthNumber == 2){
-  //   print('February');
-  // }
-  // else if( monthNumber == 12 ){
-  //   print('December');
-  // }
-  // else{
-  //   print('Invalid');
-  // }
-
-  // int number, remainder;
-  // print('Enter a number : ');
-  // number = int.parse( stdin.readLineSync()! );
-  //
-  // remainder = number % 2;
-  //
-  // (remainder == 0) ? print('Even') : print('Odd');
-
-  // int i = 1;
-  //
-  // while( i <= 10 )
-  // {
-  //   print('Pakistan $i');
-  //   i++;
-  // }
-  //
-  // print('End');
-
-  // int n1, n2;
-  // String choice;
-  //
-  // do {
-  //   print('Enter first number : ');
-  //   n1 = int.parse(stdin.readLineSync()!);
-  //   print('Enter second number : ');
-  //   n2 = int.parse(stdin.readLineSync()!);
-  //
-  //   print("Sum = ${n1 + n2}");
-  //   print('Do you want more additions(yes/no) ? ');
-  //   choice = stdin.readLineSync()!;
-  // } while (choice == 'yes');
-  //
-  // print('Thank you for using our calculator');
-
-  // List<String> studentList = ["Haleem Ullah", "Musaddiq"];
-  // studentList.add("Shaheer");
-  // studentList.add('Usman');
-  // studentList.add("Abdus Samad");
-  // studentList.add("Abu Bakar");
-  // studentList.add("Kiran");
-  //
-  // for ( int i = 0; i < studentList.length; i++ ){
-  //   print(studentList[i]);
-  // }
-  // print('*********************');
-  //
-  // for( String s in studentList){
-  //   print(s);
-  // }
+void printStars() => print('***********************************');
 
 
-  // Map<String, int> sscMarks = {
-  //   'Haleem': 765,
-  //   'Musaddiq': 766,
-  // };
-  //
-  // print(sscMarks['Musaddiq']);
+void drawLine( String symbol)
+{
+  for( int i = 1; i < 40; i++ ){
+    stdout.write(symbol);
+  }
 
-  Map<String, dynamic> playerInfo = {
-    'name': 'Babar Azam',
-    'jersey': 56,
-    'married': false,
-    'country': 'Pakistan',
-    'odiAverage': 88.3,
-    'last_5_innings': [ 44, 34, 23, 99, 34],
-  };
-
-  Map<String, dynamic> player2Info = {
-    'name': 'Virat Kohli',
-    'jersey': 99,
-    'married': true,
-    'country': 'India',
-    'odiAverage': 98.3,
-    'last_5_innings': [ 144, 34, 23, 99, 34],
-  };
-
-  List<Map<String, dynamic>> top5Players = [
-    playerInfo,
-    player2Info,
-    {
-      'name': 'M Rizwan',
-      'jersey': 23,
-      'married': true,
-      'country': 'Pakistan',
-      'odiAverage': 98.3,
-      'last_5_innings': [ 144, 34, 23, 99, 34],
-    }
-
-  ];
+  print('');
 }
+
+void sum( int a, int b) => print( 'Sum = ${a+b}');
+
+// positional parameters
+// named parameters
+void table({ required int value, int range = 10} ) // called function
+{
+  for( int i = 1; i <= range; i++ ){
+    print('$value X $i = ${value*i}');
+  }
+
+  printStars();
+  print('Table Written');
+
+}
+
+double square( double value)
+{
+  return value*value;
+}
+
+// Boss
+  void main() { // calling function
+  String name;
+
+  Student ali;
+  ali = Student();
+
+  var bilal = Student();
+
+  ali.name = "Muhammad Ali";
+  ali.mobile = "838838338";
+  ali.uni = "KUST";
+  ali.cnic  = "27272727272727";
+
+
+  bilal.name = "M Bilal";
+  bilal.mobile = '03838838383';
+
+
+  Course flutter = Course(); // default constructor
+  flutter.name = "Flutter App Dev";
+  flutter.duration = "3 Months";
+  flutter.instructor = "Muhammad Abid";
+  print(flutter.name);
+
+  Course webDev = Course();
+
+
+  List<Student> studentList = [ali, bilal];
+
+  for( Student s in studentList){
+    s.display();
+  }
+}
+
+
+// camelCase
+
+// PascalCase
+class Student
+{
+   String name = '';
+  late String mobile;
+  String? uni;
+  String? cnic;
+
+  void display() {
+    print('Name $name');
+    print('Mobile $mobile');
+    print('Uni $uni');
+    print('CNIC $cnic');
+  }
+}
+
+class Course
+{
+  late String name;
+  late String duration;
+  late String instructor;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
