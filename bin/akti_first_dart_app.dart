763dc3f0813dcc12e0ac9df37c9c97a09d78a27e
Abid <abidroid1@gmail.com>
2024-11-24 12:37:47 +0500

    named parameters in constructor

--- bin/akti_first_dart_app.dart
+++ bin/akti_first_dart_app.dart
@@ -7,72 +7,71 @@
   Dart program
  */
 
-
-
 import 'dart:io';
 
 void printStars() => print('***********************************');
 
-
-void drawLine( String symbol)
-{
-  for( int i = 1; i < 40; i++ ){
+void drawLine(String symbol) {
+  for (int i = 1; i < 40; i++) {
     stdout.write(symbol);
   }
 
   print('');
 }
 
-void sum( int a, int b) => print( 'Sum = ${a+b}');
+void sum(int a, int b) => print('Sum = ${a + b}');
 
 // positional parameters
 // named parameters
-void table({ required int value, int range = 10} ) // called function
+void table({required int value, int range = 10}) // called function
 {
-  for( int i = 1; i <= range; i++ ){
-    print('$value X $i = ${value*i}');
+  for (int i = 1; i <= range; i++) {
+    print('$value X $i = ${value * i}');
   }
 
   printStars();
   print('Table Written');
-
 }
 
-double square( double value)
-{
-  return value*value;
+double square(double value) {
+  return value * value;
 }
 
 // Boss
-  void main() { // calling function
+void main() {
+  // calling function
   Student ali;
-  ali = Student("Ali", "03838838383", null, null); // default constructor call
+  ali = Student(name: "Ali", mobile: "03838838383"); // default constructor call
+
   ali.display();
-    Student bilal = Student("Bilal", "038838383", "SUIT", "173838383838");
+  Student bilal = Student(
+      name: "Bilal", mobile: "038838383", uni: "SUIT", cnic: "173838383838,");
   bilal.display();
 
   Student khan = Student.withJustName('Khan');
+
+  Student zia = Student(name: "Zia Khan", mobile: "9398383838383");
 }
 
-class Student
-{
+class Student {
   late String name;
   late String mobile;
   String? uni;
   String? cnic;
 
   // parameterized constructor
-  Student(String n, String m, String? u, String? c) {
-    name = n;
-    mobile = m;
-    uni = u;
-    cnic = c;
-  }
+  Student({
+    required this.name,
+    required this.mobile,
+    this.uni,
+    this.cnic,
+  });
 
   // named constructor
-  Student.withJustName(String n){
+  Student.withJustName(String n) {
     name = n;
   }
+
   // method
   void display() {
     print('Name $name');
@@ -82,25 +81,8 @@ class Student
   }
 }
 
-class Course
-{
+class Course {
   late String name;
   late String duration;
   late String instructor;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
